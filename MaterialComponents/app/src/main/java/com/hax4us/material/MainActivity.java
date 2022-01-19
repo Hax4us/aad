d45862c32326aa31a66fd55cc4a2fcf246be707a
Hax4Us <lkpandey950@gmail.com>
2022-01-19 17:18:30 +0530

    MaterialComponents #0

---
+++ MaterialComponents/app/src/main/java/com/hax4us/material/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hax4us.material;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
